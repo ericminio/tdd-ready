8340ce74a979cfe62312f32d651a0e39d3158f6d
Eric Mignot <eric.mignot@gmail.com>
2018-10-04 12:03:44 -0700

    e2e tests now start the server

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,11 +1,23 @@
+const { exec } = require('child_process');
+
+let server;
 module.exports = {
-  'default e2e tests': browser => {
-    browser
-      .url(process.env.VUE_DEV_SERVER_URL)
-      .waitForElementVisible('#app', 5000)
-      .assert.elementPresent('.hello')
-      .assert.containsText('#title', 'Hello Vue')
-      .assert.elementCount('img', 1)
-      .end()
-  }
+
+    before: ()=> {
+        console.log('starting server...')
+        server = exec("python -c 'import api; api.app.run(port=8082)'")
+    },
+    after: ()=> {
+        console.log('stopping server...')
+        server.kill('SIGHUP')
+    },
+    'default e2e tests': browser => {
+        browser
+            .url(process.env.VUE_DEV_SERVER_URL)
+            .waitForElementVisible('#app', 5000)
+            .assert.elementPresent('.hello')
+            .assert.containsText('#title', 'Hello Vue')
+            .assert.elementCount('img', 1)
+            .end()
+    }
 }
