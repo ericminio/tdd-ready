f7b388850e3fa569bab7b77992ef0dec3e600450
Eric Mignot <eric.mignot@gmail.com>
2018-10-05 11:32:43 -0700

    unit test now acknowledge the use of vuex

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -3,21 +3,32 @@ import Vue from 'vue'
 import axios from 'axios';
 import sinon from 'sinon';
 import HelloWorld from '@/components/HelloWorld.vue'
+import store from '@/store'
 
 describe('HelloWorld.vue', () => {
 
+    let sandbox;
+    let component;
+    beforeEach(() => {
+        sandbox = sinon.createSandbox()
+        component = new Vue(HelloWorld)
+        component.$store = store
+    })
+    afterEach(() => sandbox.restore())
+
     it('sets a default message', () => {
-        expect(HelloWorld.data().message).to.equal('loading...')
+        sandbox.stub(axios, 'get').returns(new Promise((r) => r({})));
+        let vm = component.$mount()
+
+        expect(vm.$el.querySelector('#title').textContent).to.equal('loading...')
     })
 
-    let sandbox;
-    beforeEach(() => sandbox = sinon.createSandbox())
-    afterEach(() => sandbox.restore())
+
 
     it('displays the fetched message', (done) => {
         const resolved = new Promise((resolve) => resolve({ data: { message:'this fetched message'} }));
         sandbox.stub(axios, 'get').returns(resolved);
-        const vm = new Vue(HelloWorld).$mount()
+        let vm = component.$mount()
 
         setTimeout(() => {
             expect(vm.$el.querySelector('#title').textContent).to.equal('this fetched message')
@@ -28,7 +39,7 @@ describe('HelloWorld.vue', () => {
     it('displays error message if any', (done)=>{
         const rejected = new Promise((resolve, reject) => reject('too bad'));
         sandbox.stub(axios, 'get').returns(rejected);
-        const vm = new Vue(HelloWorld).$mount()
+        let vm = component.$mount()
 
         setTimeout(() => {
             expect(vm.$el.querySelector('#title').textContent).to.equal('too bad')
